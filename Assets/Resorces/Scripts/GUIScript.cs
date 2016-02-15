fd890c30b61202dd7e8cbc2d35f55419f3a8e1ae
Richard Greaves <roboritch@gmail.com>
2016-02-15 11:17:38 -0800

    did some work on unit buttons and the unit action queue
    also updated some meta files and the scene

--- Assets/Resorces/Scripts/GUIScript.cs
+++ Assets/Resorces/Scripts/GUIScript.cs
@@ -3,15 +3,58 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class GUIScript : MonoBehaviour {
-	public Button[] buttons;
-	private int MAX_BUTTONS = 8;
 
-	// Use this for initialization
-	void Start() {
-		buttons = new Button[MAX_BUTTONS];
-
+	#region unit selection
+	private UnitScript currentlySelectedUnit;
+	/// <summary>
+	/// Sets the unit as selected.
+	/// The gridblock should call this after it is clicked
+	/// Passing null will deselect a unit
+	/// </summary>
+	/// <param name="u">Unit.</param>
+	public void setUnitAsSelected(UnitScript u){
+		currentlySelectedUnit = u;
 	}
-	
+	public UnitScript getCurUnit(){
+		return currentlySelectedUnit;
+	}
+	#endregion
+
+	#region buttons
+	//all this must be set in the inspecter
+	//if there are null pointer exeptions check the refrences in unity
+	public Button[] buttons; // buttons the unit passes to this script
+	private int MAX_BUTTONS = 6;
+	public Button resetUnitActions;
+	public void resetUnitAction(){
+		//currentlySelectedUnit.resetActionQueue(); TODO get this working in unitScript
+	}
+
+
+	#endregion
+
+	#region unit infromation text var
+	//the currently selected unit will change these themselves
+	public defaultTextHolder attack;
+	public defaultTextHolder currentSize;
+	public defaultTextHolder maxSize;
+	public defaultTextHolder moveActions;
+	public defaultTextHolder extraInfo1;
+	public defaultTextHolder extraInfo2;
+	public defaultTextHolder extraInfo3;
+	#endregion
+
+
+	//TODO setup unit acting
+	#region unit acting
+
+	#endregion
+
+	void Start() {
+		
+	}
+
+
 	// Update is called once per frame
 	void Update() {
 	
---
+++ Assets/Resorces/Scripts/UnitActingScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <summary>
+/// Unit acting script.
+/// Many of these are controled by the game gui script
+/// </summary>
+public class UnitActingScript : MonoBehaviour {
+	public Image unitColor;
+	public Image unitHeadSprite;
+	public bool enemy; // TODO add some identifyer for enemy units
+
+	public void setUnitSprite(Sprite unitHead,Color color){
+		unitColor.color = color;
+		unitHeadSprite.sprite = unitHead;
+	}
+		
+	public void setCurrentlyActing(){
+		//TODO add some animation to show that this unit is acting 
+	}
+		
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
