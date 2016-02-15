333dc4b3caa95eae4436e363d7b3dcf9e6dc38a8
Richard Greaves <roboritch@gmail.com>
2016-02-15 09:12:41 -0800

    started gui script

---
+++ Assets/Resorces/Scripts/GUIScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GUIScript : MonoBehaviour {
+	public Button[] buttons;
+	private int MAX_BUTTONS = 8;
+
+	// Use this for initialization
+	void Start() {
+		buttons = new Button[MAX_BUTTONS];
+
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	
+	}
+}
