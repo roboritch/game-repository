029fd3d62e3ab25908010adf14f08a9b010af202
Richard Greaves <roboritch@gmail.com>
2016-02-21 11:57:15 -0800

    started unit selection

---
+++ Assets/Resorces/Scripts/UnitSelectionScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Gets all avalible units from the current level
+/// </summary>
+public class UnitSelectionScript : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start() {
+		
+	}
+}
