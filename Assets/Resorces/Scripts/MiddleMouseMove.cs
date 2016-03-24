fefe7f2bbbe450d6db1dbfd401d3cecb8e07fd5d
Brian Matte <brianmatte@gmail.com>
2016-03-24 12:35:11 -0700

    MiddleMouseMove Cleanup
    Cleaned up the MiddleMouseMove document. Removed the region due to there
    only being ~60 lines of code in the document.

--- Assets/Resorces/Scripts/MiddleMouseMove.cs
+++ Assets/Resorces/Scripts/MiddleMouseMove.cs
@@ -1,60 +1,61 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MiddleMouseMove : MonoBehaviour {
-	public float scrollingSpeed;
+public class MiddleMouseMove : MonoBehaviour{
+  public float scrollingSpeed;
 
-	public bool invertX;
-	public bool invertY;
+  public bool invertX;
+  public bool invertY;
 
-	private float lPosX;
-	private float lPosY;
-	private float cPosX;
-	private float cPosY;
+  private float lPosX;
+  private float lPosY;
+  private float cPosX;
+  private float cPosY;
 
-	public Camera cam;
-	public float zoomSpeed;
-	public bool invertZoom;
+  public Camera cam;
+  public float zoomSpeed;
+  public bool invertZoom;
 
-	// Use this for initialization
-	void Start() {
-		cPosX = Input.mousePosition.x;
-		cPosY = Input.mousePosition.y;
-		lPosX = Input.mousePosition.x;
-		lPosY = Input.mousePosition.y;
-	}
+  // Use this for initialization.
+  /// <summary>
+  /// Start this instance.
+  /// </summary>
+  void Start(){
+    cPosX = Input.mousePosition.x;
+    cPosY = Input.mousePosition.y;
+    lPosX = Input.mousePosition.x;
+    lPosY = Input.mousePosition.y;
+  }
 	
-	// Update is called once per frame
-	void Update() {
+  // Update is called once per frame.
+  /// <summary>
+  /// Update this instance.
+  /// </summary>
+  void Update(){
+    cPosX = Input.mousePosition.x;
+    cPosY = Input.mousePosition.y;
 
-		#region mouse scrolling
-		cPosX = Input.mousePosition.x;
-		cPosY = Input.mousePosition.y;
+    if (Input.GetMouseButtonDown(2) || Input.GetMouseButton(2)){
 
-		if(Input.GetMouseButtonDown(2) || Input.GetMouseButton(2)) {
+      //Lots of code but best implementation.
+      //Invertions can be changed at any time.
+      if (invertX && invertY){
+        transform.Translate(-(cPosX - lPosX) * scrollingSpeed, -(cPosY - lPosY) * scrollingSpeed, 0f);
+      } else if (invertX){
+        transform.Translate(-(cPosX - lPosX) * scrollingSpeed, (cPosY - lPosY) * scrollingSpeed, 0f);
+      } else if (invertY){
+        transform.Translate((cPosX - lPosX) * scrollingSpeed, -(cPosY - lPosY) * scrollingSpeed, 0f);
+      } else{
+        transform.Translate((cPosX - lPosX) * scrollingSpeed, (cPosY - lPosY) * scrollingSpeed, 0f);
+      }
+    }
+    if (invertZoom){
+      cam.orthographicSize += zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
+    } else{
+      cam.orthographicSize -= zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
+    }
 
-			//lots of code but best implementation
-			//invertions can be changed at any time
-			if(invertX && invertY) {
-				transform.Translate(-(cPosX - lPosX) * scrollingSpeed, -(cPosY - lPosY) * scrollingSpeed, 0f);
-			} else if(invertX) {
-				transform.Translate(-(cPosX - lPosX) * scrollingSpeed, (cPosY - lPosY) * scrollingSpeed, 0f);
-			} else if(invertY) {
-				transform.Translate((cPosX - lPosX) * scrollingSpeed, -(cPosY - lPosY) * scrollingSpeed, 0f);
-			} else {
-				transform.Translate((cPosX - lPosX) * scrollingSpeed, (cPosY - lPosY) * scrollingSpeed, 0f);
-			}
-		}
-		if(invertZoom) {
-			cam.orthographicSize += zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
-		} else {
-			cam.orthographicSize -= zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
-		}
-
-		lPosX = cPosX;
-		lPosY = cPosY;
-
-		#endregion
-
-	}
+    lPosX = cPosX;
+    lPosY = cPosY;
+  }
 }
