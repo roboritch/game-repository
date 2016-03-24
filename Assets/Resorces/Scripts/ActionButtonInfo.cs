92c15001f8171921a8b051870737fe779c31d76c
Brian Matte <brianmatte@gmail.com>
2016-03-24 11:17:27 -0700

    Formatting
    Formatted ActionButtonInfo and ActionScript documents.

--- Assets/Resorces/Scripts/ActionButtonInfo.cs
+++ Assets/Resorces/Scripts/ActionButtonInfo.cs
@@ -6,17 +6,18 @@ using System.Collections;
 /// All new actions must create their own child of this script.
 /// </summary>
 public abstract class ActionButtonInfo : MonoBehaviour{
-	[SerializeField] protected string actionName;
-	[SerializeField] protected TextAsset discription;
-	/// <summary>
-	/// Gets the new instance of action.
-	/// The action type must be set through a new child of this object.
-	/// </summary>
-	/// <returns>The new instance of action.</returns>
-    public abstract ActionScript getNewInstanceOfAction(UnitScript unit);
-		
-	public string getDescriptionText(){
-		return discription.text;
-	}
+  [SerializeField] protected string actionName;
+  [SerializeField] protected TextAsset discription;
+
+  /// <summary>
+  /// Gets the new instance of action.
+  /// The action type must be set through a new child of this object.
+  /// </summary>
+  /// <returns>The new instance of action.</returns>
+  public abstract ActionScript getNewInstanceOfAction(UnitScript unit);
+
+  public string getDescriptionText(){
+    return discription.text;
+  }
 
 }
--- Assets/Resorces/Scripts/ActionScript.cs
+++ Assets/Resorces/Scripts/ActionScript.cs
@@ -9,58 +9,63 @@ using System.IO;
 /// and executed in sequence with other actions.
 /// </summary>
 public abstract class ActionScript{
-	/// <summary>The unit performing this action. </summary>
-	protected UnitScript unit;
-	protected float actionTime;
+  /// <summary>The unit performing this action. </summary>
+  protected UnitScript unit;
+  protected float actionTime;
   /// <summary>Some actions may require more than one action Id. </summary>
-	private int actionID;
-	//Set for each display of the action
+  private int actionID;
+  //Set for each display of the action
   //each move. Attack or action must be a seprate instance of this script.
 
-	public int ActionID {
-		get {
-	return actionID;
-	}
-	set {
-		actionID = value;
-		}
-	}
+  public int ActionID {
+    get {
+      return actionID;
+    }
+    set {
+      actionID = value;
+    }
+  }
 
 
-	protected ActionScript(UnitScript u){
-		unit = u;
-	}
+  protected ActionScript(UnitScript u){
+    unit = u;
+  }
 
 
 
-	/// <summary> Perform this action when called by the unit's action list. </summary>
-	public abstract void act();
+  /// <summary> Perform this action when called by the unit's action list. </summary>
+  public abstract void act();
 
-	/// <summary>The amount of time it takes to animate the unit preforming this action.</summary>
+  /// <summary>The amount of time it takes to animate the unit preforming this action.</summary>
   /// <returns>The time.</returns>
-	public virtual float getActionTime(){
-		return actionTime;
-	}
+  public virtual float getActionTime(){
+    return actionTime;
+  }
 
-	/// <summary>Calls the GUI to display this action's posible outputs on the game. </summary>
-	public abstract void displayUserSelection();
-	/// <summary> Displaies the finished action that the user has chosen. </summary>
-	public abstract void displayFinishedAction();
-	/// <summary>Calls this to get the GUI to remove displayed images during reset or when action is compleated.</summary>
-	public abstract void removeActionRepresentationDisplay();
-	/// <summary> Removes the user selection display this can be called when the action is selected or when it is canceld. </summary>
-	public abstract void removeUserSelectionDisplay();
-	/// <summary>
-	/// This is called when a user has selected their action location.
-	/// </summary>
-	/// <param name="blockSelected">Block selected.</param>
-	public abstract void userSelectedAction(GridBlock blockSelected);
+  /// <summary>Calls the GUI to display this action's posible outputs on the game. </summary>
+  public abstract void displayUserSelection();
 
-	public abstract SerializedCompeatedAction serializeAction();
+  /// <summary> Displaies the finished action that the user has chosen. </summary>
+  public abstract void displayFinishedAction();
 
-	public abstract void loadAction(SerializedCompeatedAction s);
+  /// <summary>Calls this to get the GUI to remove displayed images during reset or when action is compleated.</summary>
+  public abstract void removeActionRepresentationDisplay();
+
+  /// <summary> Removes the user selection display this can be called when the action is selected or when it is canceld. </summary>
+  public abstract void removeUserSelectionDisplay();
+
+  /// <summary>
+  /// This is called when a user has selected their action location.
+  /// </summary>
+  /// <param name="blockSelected">Block selected.</param>
+  public abstract void userSelectedAction(GridBlock blockSelected);
+
+  public abstract SerializedCompeatedAction serializeAction();
+
+  public abstract void loadAction(SerializedCompeatedAction s);
 
 }
+
 /// <summary>
 /// Each action queue must be sent to the other players using serealizable information.
 /// each action must save and load there information using this struct.
@@ -68,7 +73,7 @@ public abstract class ActionScript{
 /// </summary>
 [System.Serializable]
 public struct SerializedCompeatedAction{
-	public GridLocation locationToPreformAction;
-	public int actionAmountInt;
-	public System.Type actionType;
+  public GridLocation locationToPreformAction;
+  public int actionAmountInt;
+  public System.Type actionType;
 }
