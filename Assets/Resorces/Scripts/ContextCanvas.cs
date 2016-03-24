e24d7123780be3fd871ffb0c644d925d816ac167
Brian Matte <brianmatte@gmail.com>
2016-03-24 11:33:02 -0700

    Maintained ContextCanvas
    Commented and formatted ContextCanvas.

--- Assets/Resorces/Scripts/ContextCanvas.cs
+++ Assets/Resorces/Scripts/ContextCanvas.cs
@@ -2,44 +2,51 @@
 using System.Collections;
 using UnityEngine.UI;
 
-
 /// <summary>
-/// used by the "CreatePlayGrid" script to help with level editing.
+/// Used by the "CreatePlayGrid" script to help with level editing.
 /// </summary>
-public class ContextCanvas : MonoBehaviour {
-	//these must be set in the prefab
-	public Button removeSpace;
-	public Button makeSpawnPoint;
-	//the context object
-	public GridBlock space;
+public class ContextCanvas : MonoBehaviour{
+  /// <summary>
+  /// The button to remove a space.
+  /// </summary>
+  public Button removeSpace;
+  /// <summary>
+  /// The button to make a spawn point.
+  /// </summary>
+  public Button makeSpawnPoint;
+  /// <summary>
+  /// The context object.
+  /// </summary>
+  public GridBlock space;
 
-	// Use this for initialization
-	void Start() {
+  // Use this for initialization.
+  /// <summary>
+  /// Start this instance.
+  /// </summary>
+  void Start(){
+  }
 
+  /// <summary>
+  /// Allow programs to be spawned from this block.
+  /// </summary>
+  public void MakeSpawnPoint(){
+    space.setSpawn();
+  }
 
-	}
+  /// <summary>
+  /// Removes the space from the game grid.
+  /// </summary>
+  public void RemoveSpace(){
+    space.toggleSpaceAvailable();
+  }
 
-	/// <summary>
-	/// alow programes to be spawned from this block
-	/// </summary>
-	public void MakeSpawnPoint() {
-		space.setSpawn();
-	}
-
-	/// <summary>
-	/// removes the space from the game grid
-	/// </summary>
-	public void RemoveSpace() {
-		space.toggleSpaceAvailable();
-	}
-
-	/// <summary>
-	/// destroy this context menu
-	/// </summary>
-	public void ExitContextMenu() { //TODO setup close button in prefab
-		space.ExitContextMenu();
-		Destroy(gameObject);
-	}
+  /// <summary>
+  /// Destroy this context menu.
+  /// </summary>
+  public void ExitContextMenu(){ //TODO setup close button in prefab
+    space.ExitContextMenu();
+    Destroy(gameObject);
+  }
 
 }
 
