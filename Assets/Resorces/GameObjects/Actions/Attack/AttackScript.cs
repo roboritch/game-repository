14a2ac12432c8428ee68e3a1868be8a67b10a2ec
Richard Greaves <roboritch@gmail.com>
2016-03-25 11:41:19 -0700

    Revert "updated prefs  for monodovelop formating"
    This reverts commit 8cf7abdb8c932ed4a7139ae47b4e655a576356f1.

--- Assets/Resorces/GameObjects/Actions/Attack/AttackScript.cs
+++ Assets/Resorces/GameObjects/Actions/Attack/AttackScript.cs
@@ -4,54 +4,52 @@ using System.Collections;
 /// <summary> Attack script.</summary>
 public class AttackScript : ActionScript{
 
-	public AttackScript(UnitScript u) : base(u){
+	public AttackScript (UnitScript u) : base (u){
 		attackStrength = unit.getAttackPower();
 	}
 
 	#region attack strength and location
-
 	/// <summary>
-	/// The attack grid block location.
-	/// </summary>
+  /// The attack grid block location.
+  /// </summary>
 	private GridBlock attackLocation;
 
 	/// <summary>
-	/// The attack strength; the number of blocks removed by this attack.
-	/// </summary>
+ 	/// The attack strength; the number of blocks removed by this attack.
+ 	/// </summary>
 	private int attackStrength = 0;
 
 	/// <summary>
-	/// Sets the attack location.
-	/// </summary>
-	/// <param name="attackLocation">Attack location.</param>
+	 /// Sets the attack location.
+ 	/// </summary>
+ 	/// <param name="attackLocation">Attack location.</param>
 	private void setAttackLocation(GridBlock attackLocation){
 		this.attackLocation = attackLocation;
 	}
 
 	/// <summary>
-	/// Gets the attack location.
-	/// </summary>
-	/// <returns>The attack location.</returns>
+  /// Gets the attack location.
+  /// </summary>
+  /// <returns>The attack location.</returns>
 	public GridBlock getAttackLocation(){
 		return attackLocation;
 	}
 
 	/// <summary>
-	/// Sets the attack strength.
-	/// </summary>
-	/// <param name="attackStrength">Attack strength.</param>
+  /// Sets the attack strength.
+  /// </summary>
+  /// <param name="attackStrength">Attack strength.</param>
 	public void setAttackStrength(int attackStrength){
 		this.attackStrength = attackStrength;
 	}
 
 	/// <summary>
-	/// Gets the attack strength.
-	/// </summary>
-	/// <returns>The attack strength.</returns>
+  /// Gets the attack strength.
+  /// </summary>
+  /// <returns>The attack strength.</returns>
 	public int getAttackStrength(){
 		return attackStrength;
 	}
-
 	#endregion
 
 	/// <summary>
@@ -60,15 +58,15 @@ public class AttackScript : ActionScript{
 	public override void act(){
 		removeActionRepresentationDisplay();
 		if(attackLocation != null)
-		if(attackLocation.unitInstalled != null){
+		if (attackLocation.unitInstalled != null){
 			if(attackLocation.isAdj(unit.getCurrentBlockHeadLocation())){
 				displayCloseRangeAttackAnimation(attackLocation);
-				attackLocation.unitInstalled.queueBlockRemoval(attackStrength, actionTime, 0f); // must be called after the display
-			} else{
+				attackLocation.unitInstalled.queueBlockRemoval(attackStrength,actionTime,0f); // must be called after the display
+			}else{
 				float delay = displayLongRangeAttackAnimation(attackLocation);
-				attackLocation.unitInstalled.queueBlockRemoval(attackStrength, actionTime, delay);
+				attackLocation.unitInstalled.queueBlockRemoval(attackStrength,actionTime,delay);
 			}
-		} else{
+		}else{
 			Debug.Log("unit's block was removed before it could be attacked");
 		}
 	}
@@ -76,9 +74,9 @@ public class AttackScript : ActionScript{
 	private void displayCloseRangeAttackAnimation(GridBlock animationLocation){
 		GameObject closeAttack = unit.instantiationHelper(unit.grid.getAnimation("close attack"));
 		actionTime = closeAttack.GetComponent<IGetAnimationTimeToFin>().getAnimationTime();
-		closeAttack.transform.SetParent(animationLocation.transform, false);
+		closeAttack.transform.SetParent(animationLocation.transform,false);
 		closeAttack.transform.localPosition = new Vector3();
-		closeAttack.transform.position = closeAttack.transform.position + new Vector3(0f, 1f, 0f);
+		closeAttack.transform.position = closeAttack.transform.position + new Vector3(0f,1f,0f);
 	}
 
 
@@ -87,21 +85,20 @@ public class AttackScript : ActionScript{
 		GameObject farAttackOut = unit.instantiationHelper(unit.grid.getAnimation("far attack out")); // diplayed on the units block
 		GameObject farAttackIn = unit.instantiationHelper(unit.grid.getAnimation("far attack in")); // diplayed on the animationLocation
 		float delay = farAttackOut.GetComponent<IGetAnimationTimeToFin>().getAnimationTime();
-		actionTime = farAttackIn.GetComponent<IGetAnimationTimeToFin>().getAnimationTime();
-		farAttackOut.transform.SetParent(unit.getCurrentBlockHeadLocation().transform, false);
+		actionTime =farAttackIn.GetComponent<IGetAnimationTimeToFin>().getAnimationTime();
+		farAttackOut.transform.SetParent(unit.getCurrentBlockHeadLocation().transform,false);
 		farAttackOut.transform.localPosition = new Vector3();
-		farAttackOut.transform.position = farAttackOut.transform.position + new Vector3(0f, 1f, 0f);
+		farAttackOut.transform.position = farAttackOut.transform.position + new Vector3(0f,1f,0f);
 
-		farAttackIn.transform.SetParent(animationLocation.transform, false);
+		farAttackIn.transform.SetParent(animationLocation.transform,false);
 		farAttackIn.transform.localPosition = new Vector3();
-		farAttackIn.transform.position = farAttackOut.transform.position + new Vector3(0f, 1f, 0f);
+		farAttackIn.transform.position = farAttackOut.transform.position + new Vector3(0f,1f,0f);
 
 		return delay;
 	}
 
 
 	#region user display
-
 	/// <summary>
 	/// Calls the GUI to display this action on the game.
 	/// </summary>
@@ -116,9 +113,7 @@ public class AttackScript : ActionScript{
 	}
 
 	#region set posible attack locations
-
 	private GridBlock[] posibleAttackLocations;
-
 	/// <summary>
 	/// Sets the posible attack locations.
 	/// Valid locations will be flound within this script
@@ -135,26 +130,24 @@ public class AttackScript : ActionScript{
 	#endregion
 
 	private void checkAndDisplayPossibleUserActions(){
-		for(int i = 0; i < posibleAttackLocations.Length; i++){
-			posibleAttackLocations[i].attackActionWantsToAttackHere(this, unit);
+		for (int i = 0; i < posibleAttackLocations.Length; i++) {
+			posibleAttackLocations[i].attackActionWantsToAttackHere(this,unit);
 		}
 	}
+		
 
-
-	public override void removeUserSelectionDisplay(){
+	public override void removeUserSelectionDisplay () {
 		if(posibleAttackLocations != null)
-			for(int i = 0; i < posibleAttackLocations.Length; i++){
-				posibleAttackLocations[i].removeAttackDisplayForThis();
-			}
+		for (int i = 0; i < posibleAttackLocations.Length; i++) {
+			posibleAttackLocations[i].removeAttackDisplayForThis();
+		}
 	}
 
 	#endregion
 
 	#region finished action display
-
-	private int actionDisplayID = -1;
-	//for when enamy actions are loaded
-	public override void displayFinishedAction(){
+	private int actionDisplayID = -1; //for when enamy actions are loaded
+	public override void displayFinishedAction () {
 		actionDisplayID = attackLocation.spriteDisplayScript.displayAction(unit.grid.spritesAndColors.sprite_attack);
 	}
 
@@ -166,11 +159,10 @@ public class AttackScript : ActionScript{
 		attackLocation.spriteDisplayScript.removeAction(actionDisplayID);
 		unit.addAttackAction();
 	}
+	#endregion 
 
-	#endregion
 
-
-	public override void userSelectedAction(GridBlock blockSelected){
+	public override void userSelectedAction (GridBlock blockSelected) {
 		removeUserSelectionDisplay(); // remove the selection display
 		attackLocation = blockSelected; // set attack location
 		displayFinishedAction(); // display the finished action
@@ -179,20 +171,18 @@ public class AttackScript : ActionScript{
 	}
 
 	#region action Save And Load
-
-	public override void loadAction(SerializedCompeatedAction s){
+	public override void loadAction (SerializedCompeatedAction s) {
 		attackStrength = s.actionAmountInt;
 		attackLocation = unit.grid.gridLocationToGameGrid(s.locationToPreformAction);
 	}
 
-	public override SerializedCompeatedAction serializeAction(){
+	public override SerializedCompeatedAction serializeAction () {
 		SerializedCompeatedAction compleatedActionSave = new SerializedCompeatedAction();
 		compleatedActionSave.actionAmountInt = attackStrength;
 		compleatedActionSave.locationToPreformAction = attackLocation.gridlocation;
 		compleatedActionSave.actionType = typeof(AttackScript);
 		return compleatedActionSave;
 	}
-
 	#endregion
 
 
