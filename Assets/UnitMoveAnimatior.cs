4c4cacaf7d7a65062deeebb5cd89903df0dfd907
Richard Greaves <roboritch@gmail.com>
2016-04-06 16:39:23 -0700

    unit moving animation

---
+++ Assets/UnitMoveAnimatior.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitMoveAnimatior : MonoBehaviour{
+	
+
+	void Start(){
+		
+	}
+
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="">.</param>
+	public void setMovmentDirection(Direction movmentDirection){
+		switch(movmentDirection){
+		case Direction.UP:
+			break;
+		case Direction.DOWN:
+			transform.Rotate(new Vector3(0, 180f, 0f));
+			break;
+		case Direction.LEFT:
+			transform.Rotate(new Vector3(0, -90f, 0f));
+			break;
+		case Direction.RIGHT:
+			transform.Rotate(new Vector3(0, 90f, 0f));
+			break;
+		default:
+			break;
+		}
+	}
+
+
+
+
+
+
+}
