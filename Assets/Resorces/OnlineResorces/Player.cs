d154d71d6ce46ed51e3c069cec359288b8fbae85
Richard Greaves <roboritch@gmail.com>
2016-04-08 14:19:31 -0700

    find level script

--- Assets/Resorces/OnlineResorces/Player.cs
+++ Assets/Resorces/OnlineResorces/Player.cs
@@ -29,5 +29,6 @@ public class Player : Singleton<Player>{
 	public NetworkIO thisPlayersNetworkHelper;
 	public bool workingOnline = false;
 	public int numberOfClientsReadyToAct;
+	public string dataPathOfLevelTheUserWantsToLoad;
 
 }
